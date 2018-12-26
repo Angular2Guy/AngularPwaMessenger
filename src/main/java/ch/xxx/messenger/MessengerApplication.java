6579732fb496e605572a35ab90f1ec7d907a6876
Angular2Guy <angular2guy@gmail.com>
2018-12-26 11:00:03 +0100

    Initial Commit

---
+++ src/main/java/ch/xxx/messenger/MessengerApplication.java
@@ -0,0 +1,14 @@
+package ch.xxx.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerApplication.class, args);
+	}
+
+}
+
