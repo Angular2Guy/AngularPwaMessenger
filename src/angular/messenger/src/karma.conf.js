0e301aa99cf8b5baf8048adab78c219382704d65
Angular2Guy <angular2guy@gmail.com>
2019-03-25 23:45:20 +0100

    travis fix

--- src/angular/messenger/src/karma.conf.js
+++ src/angular/messenger/src/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
     customLaunchers: {
         Chrome_without_security: {
           base: 'Chrome',
-          flags: ['--disable-web-security']
+          flags: ['--disable-web-security --no-sandbox']
         }
       },
     singleRun: false
