26d40b926fe559d85be18e2efc5c09a2c6f15deb
Angular2Guy <angular2guy@gmail.com>
2019-04-19 22:57:04 +0200

    add openssl step

--- src/helm/addIngress.sh
+++ src/helm/addIngress.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 minikube addons enable ingress
+#openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=minikube" -days 3650
 kubectl create secret tls minikube-tls --cert=tls.crt --key=tls.key
