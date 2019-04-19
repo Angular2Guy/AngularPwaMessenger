17d5f417c5864e1c3d57b3e152fdb6286f3987f2
Angular2Guy <angular2guy@gmail.com>
2019-04-20 00:30:48 +0200

    make cert for chrome

--- src/helm/addIngress.sh
+++ src/helm/addIngress.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 minikube addons enable ingress
-#openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=minikube" -days 3650
-kubectl create secret tls minikube-tls --cert=tls.crt --key=tls.key
+#openssl genrsa -out ca.key 2048
+#openssl req -x509 -new -key ca.key -sha256 -nodes -keyout ca.key -out ca.crt -subj "/CN=minikube" -days 3650
+#openssl genrsa -out server.key 2048
+#openssl req -new -key server.key -out server.csr -config csr.conf
+#openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extensions v3_ext -extfile csr.conf
+kubectl create secret tls minikube-tls --cert=ca.crt --key=ca.key
