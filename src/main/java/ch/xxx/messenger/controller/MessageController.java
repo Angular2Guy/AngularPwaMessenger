d79fe9471730c827ab4899ff7b9e90b5bd3109cc
Angular2Guy <angular2guy@gmail.com>
2019-05-19 08:06:43 +0200

    clean up message controller

--- src/main/java/ch/xxx/messenger/controller/MessageController.java
+++ src/main/java/ch/xxx/messenger/controller/MessageController.java
@@ -3,7 +3,6 @@ package ch.xxx.messenger.controller;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +11,12 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.xxx.messenger.dto.Contact;
 import ch.xxx.messenger.dto.Message;
 import ch.xxx.messenger.dto.SyncMsgs;
-import ch.xxx.messenger.jwt.JwtTokenProvider;
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -29,7 +26,7 @@ public class MessageController {
 	private ReactiveMongoOperations operations;
 
 	@PostMapping("/findMsgs")
-	public Flux<Message> getFindMessages(@RequestBody SyncMsgs syncMsgs, @RequestHeader Map<String, String> header) {
+	public Flux<Message> getFindMessages(@RequestBody SyncMsgs syncMsgs) {
 		List<Message> msgToUpdate = new LinkedList<>();
 		return operations
 				.find(new Query().addCriteria(Criteria.where("fromId").in(syncMsgs.getContactIds())
