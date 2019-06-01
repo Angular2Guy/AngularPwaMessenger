6e63fcaef5c9239b21e54dfe385f480883534048
Angular2Guy <angular2guy@gmail.com>
2019-06-01 10:47:02 +0200

    angular update step 3

--- src/angular/messenger/src/karma.conf.js
+++ src/angular/messenger/src/karma.conf.js
@@ -14,7 +14,10 @@ module.exports = function (config) {
 //      require('karma-phantomjs-launcher')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+    	  random: false
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
