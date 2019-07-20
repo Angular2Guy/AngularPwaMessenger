5966554a2b72a7c84a643220ae37530ebf0236a0
Angular2Guy <angular2guy@gmail.com>
2019-07-20 13:18:37 +0200

    file upload next step

--- src/angular/messenger/src/app/fileupload/fileupload.component.ts
+++ src/angular/messenger/src/app/fileupload/fileupload.component.ts
@@ -13,25 +13,56 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MainComponent } from '../main/main.component';
+import { Contact } from '../model/contact';
+import { Message } from '../model/message';
 
 @Component({
-  selector: 'app-fileupload',
-  templateUrl: './fileupload.component.html',
-  styleUrls: ['./fileupload.component.scss']
+	selector: 'app-fileupload',
+	templateUrl: './fileupload.component.html',
+	styleUrls: ['./fileupload.component.scss']
 })
 export class FileuploadComponent implements OnInit {
+	currentFile: File;
+	fileContent: string;
+	showFileSizeMsg: boolean;
+	private readonly MB: number = 1024 * 1024;
 
-  constructor(public dialogRef: MatDialogRef<MainComponent>,
-      @Inject( MAT_DIALOG_DATA ) public data: any) { }
+	constructor(public dialogRef: MatDialogRef<MainComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.showFileSizeMsg = false;
+	}
 
-  upload() {
-	
-  }
+	filechange(fileInput: any) {
+		this.currentFile = fileInput.target.files[0];
+		let reader = new FileReader();
+		reader.onload = () => {
+			let textstr: string = <string>reader.result;
+			let textstrs = textstr.split(";");
+			this.fileContent = textstrs[1];
+			//console.log(textstrs[1]);
+		};
+		reader.readAsDataURL(this.currentFile);
+	}
 
-  cancel() {
-	this.dialogRef.close(null);
-  }
+	upload() {
+		if (this.fileContent && this.fileContent.length < 2 * this.MB) {
+			let receiver = this.data.receiver as Contact;
+			let msg: Message = {
+				fromId: null,
+				toId: receiver.userId,
+				text: this.fileContent,
+				send: false,
+				received: false
+			};
+			this.dialogRef.close(msg);
+		} else {
+			this.showFileSizeMsg = true;
+		}
+	}
+
+	cancel() {
+		this.dialogRef.close(null);
+	}
 }
