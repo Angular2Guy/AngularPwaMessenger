4d5523793f11e5510e34b494546597d67dd54d3a
Angular2Guy <angular2guy@gmail.com>
2021-06-13 04:14:03 +0200

    update

--- docker.sh
+++ docker.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+#./mvnw clean install -Ddocker=true -Dnpm.test.script=test-chromium
+./mvnw clean install -Ddocker=true
 docker build -t angular2guy/angularpwamessenger:latest --build-arg JAR_FILE=messenger-0.0.1-SNAPSHOT.jar --no-cache .
+docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/angularpwamessenger:latest
