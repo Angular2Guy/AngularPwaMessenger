08ccd9015cd8a37918bb43ee6cf7df171208f052
Angular2Guy <angular2guy@gmail.com>
2021-10-17 15:05:30 +0200

    update

--- src/main/java/ch/xxx/messenger/MessengerApplication.java
+++ src/main/java/ch/xxx/messenger/MessengerApplication.java
@@ -15,10 +15,11 @@ package ch.xxx.messenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Messenger API", version = "1.0", description = "Messanger Api"))
 public class MessengerApplication {
 
 	public static void main(String[] args) {
