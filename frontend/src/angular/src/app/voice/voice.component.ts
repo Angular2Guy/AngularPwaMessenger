a599b387b751e5a47dfe612ed8b65007582d21cc
Angular2Guy <angular2guy@gmail.com>
2022-06-17 18:24:59 +0200

    docs: update

--- frontend/src/angular/src/app/voice/voice.component.ts
+++ frontend/src/angular/src/app/voice/voice.component.ts
@@ -10,6 +10,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
+ // based on: https://github.com/wliegel/youtube_webrtc_tutorial
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { VoiceService } from '../services/voice.service';
 import { VoiceMsg} from '../model/voiceMsg';
