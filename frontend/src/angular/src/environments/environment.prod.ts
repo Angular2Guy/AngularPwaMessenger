db3bd1c129a119118f647d44e1151942f550a810
Angular2Guy <angular2guy@gmail.com>
2022-07-05 11:59:56 +0200

    feat: update env

--- frontend/src/angular/src/environments/environment.prod.ts
+++ frontend/src/angular/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  onLocalhost: false,
   wsPath: 'ws://localhost:8080/signalingsocket',
   // eslint-disable-next-line @typescript-eslint/naming-convention
   RTCPeerConfiguration: {
