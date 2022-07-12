b0a0854d581fac6d65320c8af39189495a2b75e9
Angular2Guy <angular2guy@gmail.com>
2022-07-12 14:48:53 +0200

    fix: make cert work

--- helm/addIngress.sh
+++ helm/addIngress.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 minikube addons enable ingress
-#openssl genrsa -out ca.key 2048
-#openssl req -x509 -new -key ca.key -sha256 -nodes -keyout ca.key -out ca.crt -subj "/CN=minikube" -days 3650
-#openssl genrsa -out server.key 2048
-#openssl req -new -key server.key -out server.csr -config csr.conf
-#openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extensions v3_ext -extfile csr.conf
+#openssl genrsa -out rootCa.key 4096
+#openssl req -x509 -new -key rootCa.key -sha256 -nodes -keyout rootCa.key -out rootCa.pem -days 3650 -subj "/C=ry/ST=te/L=ty/O=on/OU=it/CN=minikube"
+#openssl genrsa -out ca.key 4096
+#openssl req -new -key ca.key -out ca.csr -subj "/C=ry/ST=te/L=ty/O=on/OU=it/CN=minikube"
+#openssl x509 -req -in ca.csr -CA rootCa.pem -CAkey rootCa.key -CAcreateserial -out ca.crt -days 3650 -sha256 -extfile cert.conf
+#openssl genrsa -out server.key 4096
+#openssl req -new -key server.key -out server.csr -config server.conf
+#openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extensions v3_ext -extfile server.conf
 // copy server.p12 to testCert/server.p12
 #openssl pkcs12 -export -in server.crt -inkey server.key -out server.p12 -name tomcat //password for this file(change for non test use!!!): apwamessenger
 #keytool -list -v -keystore server.p12 //check keystore
