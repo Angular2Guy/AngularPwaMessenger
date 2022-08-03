302bb6c43ccdb028e92f5f1f32605872cdbeaaa1
Angular2Guy <angular2guy@gmail.com>
2022-08-03 16:52:44 +0200

    refactor: upate

--- helm/addIngress.sh
+++ helm/addIngress.sh
@@ -8,7 +8,7 @@ minikube addons enable ingress
 #openssl genrsa -out server.key 4096
 #openssl req -new -key server.key -out server.csr -config server.conf
 #openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extensions v3_ext -extfile server.conf
-// copy server.p12 to testCert/server.p12
+#copy server.p12 to testCert/server.p12
 #openssl pkcs12 -export -in server.crt -inkey server.key -out server.p12 -name tomcat //password for this file(change for non test use!!!): apwamessenger
 #keytool -list -v -keystore server.p12 //check keystore
 kubectl create secret tls minikube-tls --cert=ca.crt --key=ca.key
