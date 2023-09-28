edccf1a3695c9450484cbd4821d1764226703582
Angular2guy <angular2guy@gmail.com>
2023-09-28 21:58:48 +0200

    feat: use bingoService

--- frontend/src/angular/src/app/games/bingo/bingo.component.ts
+++ frontend/src/angular/src/app/games/bingo/bingo.component.ts
@@ -18,10 +18,17 @@ import {
 import { BingoService } from "src/app/services/games/bingo.service";
 import { CommonModule } from "@angular/common";
 import { GamesService } from "src/app/services/games/games.service";
+import { map } from "rxjs/operators";
+import { BingoGame } from "src/app/model/games/bingo-game";
 
 export interface BingoCell{
 	value: number;
 	hit: boolean;
+}
+
+interface NewGame {
+	bingoCells: BingoCell[];
+	gameUuid: string;
 }
 
 @Component({
@@ -37,24 +44,40 @@ export interface BingoCell{
 export class BingoComponent implements OnInit, AfterViewInit {
   protected bingoCells: BingoCell[] = [];
   protected bingoNumber: number;
+  private gameUuid: string;
   
   constructor(protected gamesService: GamesService, private bingoService: BingoService) {}
   
   ngOnInit(): void {
 	this.bingoCells = [];
+    /*
     for(let y = 0;y < 5;y++) {			
 	  for(let x = 0;x < 5;x++) {
 		this.bingoCells.push({value: null,hit: false} as BingoCell);
 		this.bingoCells[y * 5 + x].hit = Math.random() > 0.5;
 		this.bingoCells[y * 5 + x].value = y * 5 + x + 1;  
 	  }
-	}    		
+	} 
+	*/   		
 	//console.log(this.gamesService.myUser);
-	this.bingoService.newGame([this.gamesService.myUser.userId]).subscribe(result => console.log(result));
+	this.bingoService.newGame([this.gamesService.myUser.userId]).pipe(map(myValue => this.mapNewGame(myValue))).subscribe(result => { this.bingoCells = result.bingoCells;
+	this.gameUuid = result.gameUuid;});
   }
 
   ngAfterViewInit(): void {
     //super.ngAfterViewInit();
   }
-      
+
+  private mapNewGame(bingoGame: BingoGame): NewGame {
+	  const myIndex = bingoGame.playerUserIds.findIndex(myValue1 => myValue1 === this.gamesService.myUser.userId);
+		const myBingoCells: BingoCell[] = [];
+	for(let y = 0;y < 5;y++) {			
+	  for(let x = 0;x < 5;x++) {
+		myBingoCells.push({value: null,hit: false} as BingoCell);
+		myBingoCells[y * 5 + x].hit = bingoGame.bingoBoards[myIndex].hits[y][x];
+		myBingoCells[y * 5 + x].value = bingoGame.bingoBoards[myIndex].board[y][x];		
+	  }
+	}
+	return {gameUuid: bingoGame.uuid, bingoCells: myBingoCells } as NewGame;
+  }      
 }
